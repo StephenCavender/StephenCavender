798f8b2a927031dd0d9dd96eeb04327b9a02c9f3
Steve <steve@pawru.com>
2022-03-16 19:33:04 -0400

    finish shields, stub latest posts

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-import Mustache from "mustache"
-import fs from "fs"
+import Mustache from "mustache";
+import fs from "fs";
+import Parser from "rss-parser";
 
-const MUSTACHE_MAIN_DIR = './main.mustache'
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+let posts = [];
+const parser = new Parser();
+(async () => {
+
+  let feed = await parser.parseURL('https://dev.cavender.io/feed.xml');
+  posts = feed.items.slice(-3)
+  console.log(feed.title);
+
+  feed.items.forEach(item => {
+    console.log(item.title + ':' + item.link);
+  });
+
+  console.log('posts', posts);
+
+})();
 
 let DATA = {
-  name: "Steve"
+  name: "Steve",
+  posts
 }
 
 const generateReadme = () => {
   fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
-    if (err) throw err
-    const output = Mustache.render(data.toString(), DATA)
-    fs.writeFileSync('README.md', output)
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
   })
 }
 
-generateReadme()
+generateReadme();
